548847d21036efb1a1737ce0706196ad86786a63
Shantanu Goel <shantanu.goel@gmail.com>
2017-10-04 13:57:50 +0530

    skeleton

---
+++ bot.py
@@ -0,0 +1,23 @@
+import logging
+import argparse
+
+from constants import VERSION
+
+def main():
+  parser = argparse.ArgumentParser(prog='deecubes-tg')
+  parser.add_argument('-v', '--version', action='version', version='%(prog)s version ' + VERSION)
+  parser.add_argument('-l', '--log', metavar='LOGLEVEL', type=int, action='store',
+                      help='Set log level. 0=> Warning, 1=>Info, 2=>Debug', default=0)
+
+  args = parser.parse_args()
+
+  if args.log >= 2:
+    log_level = logging.DEBUG
+  elif args.log == 1:
+    log_level = logging.INFO
+  else:
+    log_level = logging.WARNING
+  logging.basicConfig(level=log_level, format='%(asctime)s: %(filename)s - %(message)s')
+
+if __name__ == "__main__":
+  main()
